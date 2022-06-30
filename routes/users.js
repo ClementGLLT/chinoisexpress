36e8c63f902cba8035e256d3c2c11d7ca4a637eb
Clement Guillot <clementguillot@MacBook-Pro-de-Clement.local>
2022-06-30 11:32:46 +0200

    Modif login

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 /* Get login page*/
-router.get('/login', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res) {
+  res.render("login");
 });
 
 module.exports = router;
