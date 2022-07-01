2290d8a463aa7f68dab6dd6dab6e3bb03cf392ca
Alexis <alexis.pernet78@gmail.com>
2022-07-01 15:19:00 +0200

    popup

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+
+
+// function myFunction() {
+//   var popup = document.getElementById("myPopup");
+//   popup.classList.toggle("show");
+// }
+let popup = document.getElementById("popup");
+ function openPopup() {
+    popup.classList.add("open-popup");
+ }
+ function closePopup() {
+    popup.classList.remove("open-popup");
+ }
